045416dbdd47f10f4348f0a13dcfa1314102cab5
karthiksoma12 <153106805+karthiksoma12@users.noreply.github.com>
2026-01-22 23:49:58 +0530

    Refactor app.py for session management and routing

--- app.py
+++ app.py
@@ -48,6 +48,9 @@ else:
         show_property_page()
     
     elif st.session_state.page == "guidebook":
-        from pages.guidebook_registration import show_guidebook_page
+        from guidebook_registration import show_guidebook_page
         show_guidebook_page()
-
+    
+    elif st.session_state.page == "chat_sessions":
+        from pages.page_sessions import show_chat_sessions_page
+        show_chat_sessions_page()
